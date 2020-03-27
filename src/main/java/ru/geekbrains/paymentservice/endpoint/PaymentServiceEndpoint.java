0a4fb49e0cd874f8ae5d8e61f67ffee6547e6ca2
Vladimir <serpentcross@mail.ru>
2020-03-28 02:28:34 +0300

    Changed. Removed autowired annotation.

--- src/main/java/ru/geekbrains/paymentservice/endpoint/PaymentServiceEndpoint.java
+++ src/main/java/ru/geekbrains/paymentservice/endpoint/PaymentServiceEndpoint.java
@@ -1,14 +1,12 @@
 package ru.geekbrains.paymentservice.endpoint;
 
-
-
 import ru.geekbrains.paymentservice.repository.PaymentRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
 import ru.geekbrains.soap.payment.GetPaymentRequest;
 import ru.geekbrains.soap.payment.GetPaymentResponse;
 
@@ -17,8 +15,11 @@ public class PaymentServiceEndpoint {
 
 	private final String NAMESPACE = "http://www.geekbrains.ru/PaymentService";
 
-	@Autowired
-	private PaymentRepository paymentRepository;
+	private final PaymentRepository paymentRepository;
+
+	public PaymentServiceEndpoint(PaymentRepository paymentRepository) {
+		this.paymentRepository = paymentRepository;
+	}
 
 	@PayloadRoot(namespace = NAMESPACE, localPart = "getPaymentRequest")
 	@ResponsePayload
